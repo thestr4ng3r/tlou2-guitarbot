ce45e487baf14a5e4664efee210d05c71cdfa0dc
Florian Märkl <info@florianmaerkl.de>
2020-07-09 21:39:00 +0200

    Basic Playing

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-ChiakiLog *logger = NULL;
+ChiakiLog _logger;
+ChiakiLog *logger = &_logger;
 ChiakiSession session;
+int video_fd;
+ChiakiThread play_th;
 
 bool read_line(char *buf, size_t bufsz, FILE *f)
 {
@@ -53,7 +57,7 @@ bool read_settings(ChiakiConnectInfo *connect_info, const char *file)
 
 	READ_LINE;
 	size_t sz = sizeof(connect_info->morning);
-	if(chiaki_base64_decode(line, 0, connect_info->morning, &sz) != CHIAKI_ERR_SUCCESS
+	if(chiaki_base64_decode(line, strlen(line), connect_info->morning, &sz) != CHIAKI_ERR_SUCCESS
 			|| sz != sizeof(connect_info->morning))
 		goto beach;
 
@@ -76,19 +80,198 @@ bool read_settings(ChiakiConnectInfo *connect_info, const char *file)
 	ret = true;
 beach:
 	fclose(f);
+	return ret;
+}
+
+bool video_cb(uint8_t *buf, size_t buf_size, void *user)
+{
+	write(video_fd, buf, buf_size);
 	return true;
+}
+
+void switch_chordset(ChiakiControllerState *state, bool right)
+{
+	uint32_t button = right ? CHIAKI_CONTROLLER_BUTTON_R1 : CHIAKI_CONTROLLER_BUTTON_L1;
+	state->buttons |= button;
+	chiaki_session_set_controller_state(&session, state);
+	usleep(32000);
+	state->buttons &= ~button;
+	chiaki_session_set_controller_state(&session, state);
+}
+
+typedef enum {
+	WHEEL_TOP,
+	WHEEL_TOPR,
+	WHEEL_BOTR,
+	WHEEL_BOT,
+	WHEEL_BOTL,
+	WHEEL_TOPL
+} WheelPos;
+
+void set_wheel(ChiakiControllerState *state, WheelPos pos)
+{
+	switch(pos)
+	{
+		case WHEEL_TOP:
+			state->left_x = 0;
+			state->left_y = INT16_MIN;
+			break;
+		case WHEEL_TOPR:
+			state->left_x = INT16_MAX;
+			state->left_y = -21342;
+			break;
+		case WHEEL_BOTR:
+			state->left_x = INT16_MAX;
+			state->left_y = 21342;
+			break;
+		case WHEEL_BOT:
+			state->left_x = 0;
+			state->left_y = INT16_MAX;
+			break;
+		case WHEEL_BOTL:
+			state->left_x = INT16_MIN;
+			state->left_y = 21342;
+			break;
+		case WHEEL_TOPL:
+			state->left_x = INT16_MIN;
+			state->left_y = -21342;
+			break;
+	}
+}
+
+typedef enum {
+	STRING_NONE,
+	STRING_E,
+	STRING_a,
+	STRING_d,
+	STRING_g,
+	STRING_b,
+	STRING_e
+} String;
+
+void set_string(ChiakiControllerState *state, String string)
+{
+	state->touches[0].id = state->touches[1].id = -1;
+
+	uint16_t y = 0;
+	switch(string)
+	{
+		case STRING_NONE:
+			return;
+		case STRING_E:
+			y = 850;
+			break;
+		case STRING_a:
+			y = 750;
+			break;
+		case STRING_d:
+			y = 600;
+			break;
+		case STRING_g:
+			y = 400;
+			break;
+		case STRING_b:
+			y = 250;
+			break;
+		case STRING_e:
+			y = 50;
+			break;
+	}
+	chiaki_controller_state_start_touch(state, 1920/2, y);
+}
+
+typedef struct {
+	uint64_t duration;
+	WheelPos wheel_pos;
+	String string;
+} Note;
+
+#define DUR (300    *60/120)
+
+Note song[] = {
+	{ DUR, WHEEL_BOT,  STRING_a },
+	{ DUR, WHEEL_BOT,  STRING_d },
+	{ DUR, WHEEL_BOT,  STRING_g },
+	{ DUR, WHEEL_TOPR, STRING_g },
+
+	{ DUR, WHEEL_TOPR, STRING_E },
+	{ DUR, WHEEL_TOPR, STRING_a },
+	{ DUR, WHEEL_TOPR, STRING_d },
+	{ DUR, WHEEL_TOPR, STRING_g },
+
+	{ DUR, WHEEL_TOPL, STRING_E },
+	{ DUR, WHEEL_TOPL, STRING_a },
+	{ DUR, WHEEL_TOPR, STRING_d },
+	{ DUR, WHEEL_TOPR, STRING_g },
+
+	{ DUR, WHEEL_TOPR, STRING_E },
+	{ DUR, WHEEL_TOPR, STRING_a },
+	{ DUR, WHEEL_TOPR, STRING_d },
+	{ DUR, WHEEL_TOPR, STRING_g }
+};
+
+#define NOTES_COUNT (sizeof(song) / sizeof(Note))
+
+void play_note(ChiakiControllerState *state, Note *note)
+{
+	int16_t lx = state->left_x;
+	int16_t ly = state->left_y;
+	set_wheel(state, note->wheel_pos);
+	if(state->left_x != lx || state->left_y != ly)
+	{
+		printf("wheel\n");
+		chiaki_session_set_controller_state(&session, state);
+		usleep(32000);
+	}
+
+	printf("string\n");
+	set_string(state, note->string);
+	chiaki_session_set_controller_state(&session, state);
+	usleep(32000);
+	set_string(state, STRING_NONE);
+	chiaki_session_set_controller_state(&session, state);
+	usleep(note->duration * 1000);
+}
+
+void *play(void *user)
+{
+	(void)user;
+
+	ChiakiControllerState state;
+	chiaki_controller_state_set_idle(&state);
+
+	size_t i=0;
+	while(1)
+	{
+		play_note(&state, &song[i++]);
+		i = i % NOTES_COUNT;
+	}
+
+	return NULL;
+}
+
+void event_cb(ChiakiEvent *event, void *user)
+{
+	if(event->type == CHIAKI_EVENT_CONNECTED)
+		chiaki_thread_create(&play_th, play, NULL);
 }
 
 int main(int argc, const char *argv[])
 {
-	if(argc != 2)
+	if(argc < 2 || argc > 3)
 	{
-		printf("usage: %s [settings-file]\n", argv[0]);
+		printf("usage: %s [settings-file] ([video-fd])\n", argv[0]);
 		return 1;
 	}
 	const char *settings_file = argv[1];
+	if(argc > 2)
+		video_fd = (int)strtol(argv[2], NULL, 0);
+	else
+		video_fd = -1;
 
 	ChiakiErrorCode err;
+
+	chiaki_log_init(&_logger, CHIAKI_LOG_ALL & (~CHIAKI_LOG_VERBOSE), NULL, NULL);
 
 	ChiakiConnectInfo connect_info;
 	if(!read_settings(&connect_info, settings_file))
@@ -97,8 +280,26 @@ int main(int argc, const char *argv[])
 		return 1;
 	}
 
-	err = chiaki_session_init(&session, &connect_info, NULL);
+	err = chiaki_session_init(&session, &connect_info, logger);
+	free((void *)connect_info.host);
+	if(err != CHIAKI_ERR_SUCCESS)
+	{
+		printf("failed to init session\n");
+		return 1;
+	}
 
-	printf("TODO: do stuff now\n");
+	chiaki_session_set_video_sample_cb(&session, video_cb, NULL);
+	chiaki_session_set_event_cb(&session, event_cb, NULL);
+
+	err = chiaki_session_start(&session);
+	if(err != CHIAKI_ERR_SUCCESS)
+	{
+		printf("failed to start session\n");
+		return 1;
+	}
+
+	chiaki_session_join(&session);
+	chiaki_session_fini(&session);
+
 	return 0;
 }
